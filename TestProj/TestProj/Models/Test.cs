68dab9fb983fc08618561ac745433ba429805ed0
ACADEMY-7115VKX\Administrator <lilldeddan@gmail.com>
2017-09-19 09:51:11 +0200

    blabla

---
+++ TestProj/TestProj/Models/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProj.Models
+{
+    public class Test
+    {
+        public static string MyMethod()
+        {
+            int a = 1;
+            int b = 2;
+            return (a + b).ToString();
+        }
+    }
+}
